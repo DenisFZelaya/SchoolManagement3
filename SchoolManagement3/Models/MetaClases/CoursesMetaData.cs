6da5f5c332ed8d7b3c1087313ddea73f8301ee98
DenisFZelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-03-09 10:12:28 -0600

    Initial commit

---
+++ SchoolManagement3/Models/MetaClases/CoursesMetaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement3.Models.MetaClases
+{
+    public class CoursesMetaData
+    {
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [Range(1, 8)]
+        public int Credits { get; set; }
+    }
+
+    [MetadataType(typeof(CoursesMetaData))]
+    public partial class Course
+    {
+
+    }
+}
---
+++ SchoolManagement3/Models/MetaClases/StudentsMetaData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement3.Models.MetaClases
+{
+    public class StudentsMetaData
+    {
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Firt Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Enrollment")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Enrollment> Enrollment { get; set; }
+    }
+
+    [MetadataType(typeof(StudentsMetaData))]
+    public partial class Student { }
+}
